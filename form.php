748a7703557a8e0b8644be6bbf1a8f1cc58a8042
Hendy Budhigraha <htan1201@outlook.com>
2016-08-30 21:20:33 +0800

    making the form responsive with user experience

--- form.php
+++ form.php
@@ -5,45 +5,142 @@
 </head>
 <body>
 <?php
+    $name = '';
+    $password = '';
+    $comments = '';
+    $gender = '';
+    $tc = '';
+    $color = '';
+    $languages = array();
+
     if (isset($_POST['submit'])) {
-        // process form
-        printf('User name: %s
-            <br>Password: %s
-            <br>Gender: %s
-            <br>Color: %s
-            <br>Language(s): %s
-            <br>Comments: %s
-            <br>T&amp;C: %s',
-             $_POST['name'],
-             $_POST['password'],
-             $_POST['gender'],
-             $_POST['color'],
-             //to access array in multi-selection list
-             implode(' ', $_POST['languages']),
-             $_POST['comments'],
-             $_POST['tc']);
+        $ok = true;
+
+        if (!isset($_POST['name']) || $_POST['name'] === '') {
+            $ok = false;
+        } else {
+            $name = $_POST['name'];
+        }
+
+        if (!isset($_POST['password']) || $_POST['password'] === '') {
+            $ok = false;
+        } else {
+            $password = $_POST['password'];
+        }
+
+        if (!isset($_POST['comments']) || $_POST['comments'] === '') {
+            $ok = false;
+        } else {
+            $comments = $_POST['comments'];
+        }
+
+        if (!isset($_POST['gender']) || $_POST['gender'] === '') {
+            $ok = false;
+        }  else {
+            $gender = $_POST['gender'];
+        }
+
+        if (!isset($_POST['tc']) || $_POST['tc'] === '') {
+            $ok = false;
+        } else {
+            $tc = $_POST['tc'];
+        }
+
+        if (!isset($_POST['color']) || $_POST['color'] === '') {
+            $ok = false;
+        } else {
+            $color = $_POST['color'];
+        }
+
+        if (!isset($_POST['languages']) || !is_array($_POST['languages']) || count($_POST['languages']) === 0) {
+            $ok = false;
+        } else {
+            $languages = $_POST['languages'];
+        }
+
+        if ($ok) {
+
+            // process form
+            printf('User name: %s
+                <br>Password: %s
+                <br>Gender: %s
+                <br>Color: %s
+                <br>Language(s): %s
+                <br>Comments: %s
+                <br>T&amp;C: %s',
+                 htmlspecialchars($name, ENT_QUOTES),
+                 htmlspecialchars($password),
+                 htmlspecialchars($gender),
+                 htmlspecialchars($color),
+                 //to access array in multi-selection list
+                 htmlspecialchars(implode(' ', $languages)),
+                 htmlspecialchars($comments),
+                 htmlspecialchars($tc)
+                 );
+        }
     }
 ?>
     <form method="post" action="">
-    User Name: <input type="text" name="name"><br>
-    Password: <input type="password" name="password"><br>
+    User Name: <input type="text" name="name" value="<?php
+        echo htmlspecialchars($name); 
+    ?>"><br>
+    Password: <input type="password" name="password" value="<?php
+        echo htmlspecialchars($password); 
+    ?>"><br>
     Gender:
-        <input type="radio" name="gender" value="f">female
-        <input type="radio" name="gender" value="m">male<br>
+        <input type="radio" name="gender" value="f" <?php 
+        if ($gender === 'f') {
+            echo ' checked';
+        }
+        ?>>female
+        <input type="radio" name="gender" value="m" <?php 
+        if ($gender === 'm') {
+            echo ' checked';
+        }
+        ?>>male<br>
     Favorite color:
         <select name="color">
-            <option value="#f00">red</option>
-            <option value="#0f0">green</option>
-            <option value="#00f">blue</option>
+            <option value="">Please select</option>
+            <option value="#f00" <?php 
+            if ($color === '#f00') {
+                echo ' selected';
+            }
+            ?>>red</option>
+            <option value="#0f0" <?php 
+            if ($color === '#0f0') {
+                echo ' selected';
+            }?>>green</option>
+            <option value="#00f" <?php 
+            if ($color === '#00f') {
+                echo ' selected';
+            }?>>blue</option>
         </select><br>
     Languages spoken:
         <select name="languages[]" mutliple size="3">
-            <option value="en">English</option>
-            <option value="fr">French</option>
-            <option value="it">Italian</option>
+            <option value="en" <?php 
+             if(in_array('en', $languages)) {
+                echo ' selected';
+             }
+            ?>>English</option>
+            <option value="fr" <?php 
+             if(in_array('fr', $languages)) {
+                echo ' selected';
+             }
+            ?>>French</option>
+            <option value="it" <?php 
+             if(in_array('it', $languages)) {
+                echo ' selected';
+             }
+            ?>>Italian</option>
         </select><br>
-    Comments: <textarea name="comments"></textarea><br>
-    <input type="checkbox" name="tc" value="ok">I accept the T&#38;C<br>
+    Comments: <textarea name="comments"><?php
+        echo htmlspecialchars($comments);
+        ?></textarea><br></textarea><br>
+    <input type="checkbox" name="tc" value="ok" <?php
+        if ($tc === 'ok') {
+            echo ' checked';
+        }
+    ?>>I accept the T&#38;C<br>
     <input type="submit" name="submit" value="submit">
     </form>
 </body>
